4e19ac7d65aff2aba83f5734b688f6ecc662fbf8
Sachin-Rawat07 <sachinrawatsr07@gmail.com>
2024-04-04 03:06:25 +0530

    first-express

---
+++ 03_ExpressBasics/index.js
@@ -0,0 +1,26 @@
+// const e = require('express');
+const express=require ('express');
+const app= express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+
+app.get("/about",(req,res)=>{
+    res.send("<h1>I am the about page wass up</h1>");
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("<h1>Hello this is the contact page </h1>")
+})
+
+app.get("/services",(req,res)=>{
+    res.send("<h1> Features </h1><ol><li>Web Dev</li> <li>DSA</li></ol>   ")
+    
+
+
+
+})
+
+app.listen(3000,()=>console.log("Server is running at port 3000..."));
