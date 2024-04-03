085a23a8dee7aef9c5012105c55748657a18bda3
Sachin-Rawat07 <sachinrawatsr07@gmail.com>
2024-04-04 03:33:47 +0530

    rev

--- 03_ExpressBasics/index.js
+++ 03_ExpressBasics/index.js
@@ -17,10 +17,11 @@ app.get("/contact",(req,res)=>{
 
 app.get("/services",(req,res)=>{
     res.send("<h1> Features </h1><ol><li>Web Dev</li> <li>DSA</li></ol>   ")
-    
 
+})
 
-
+app.post("/login",(req,res)=>{
+    res.send("login success");
 })
 
 app.listen(3000,()=>console.log("Server is running at port 3000..."));
